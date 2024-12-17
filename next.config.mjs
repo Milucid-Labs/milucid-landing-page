d4261b0568f5869e9e4824ee85fe1b71dee96b8d
Kaspars Milbergs <kaspars@onthemap.com>
2024-12-17 06:43:22 +0200

    chore: update Next.js configuration and GitHub Actions workflow
    - Added output export option and distDir configuration in next.config.mjs for improved deployment handling.
    - Set basePath for production and enabled trailing slashes for URL consistency.
    - Modified GitHub Actions workflow to change permissions to write for contents and streamlined the build step to include static file preparation.
    - Updated artifact upload path to ensure correct deployment of static files.

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,13 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/milucid-landing-page' : '',
+  trailingSlash: true,
 }
 
 const withMDX = createMDX({
